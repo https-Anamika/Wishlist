dcc0332cc9434a83b9ea26d434f3cbe48f4e028d
https-Anamika <https-Anamika@github.com>
2025-04-18 10:09:45 +0530

    initial commit

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -6,19 +6,60 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var wishes: [Wish]
+    @State private var isAlertShowing: Bool = false
+    @State private var title: String = " "
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                ForEach (wishes) {  wish in
+                    Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+                   
+                }
+            }//LIST
+            .navigationTitle("Wishlist")
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing){
+                    Button{
+                        isAlertShowing.toggle()
+                    }label: {
+                        Image(systemName: "plus")
+                            .imageScale(.large)
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+            .alert("Create a new wish",isPresented: $isAlertShowing){
+                TextField("enter your wish", text: $title)
+                Button{
+                    
+                }label:{
+                    Text("Save")
+                }
+            }
+            .overlay{
+                if wishes.isEmpty{
+                    ContentUnavailableView("MY Wishlist",systemImage: "heart.circle",description: Text("No wishes yet. Add one to get started"))
+                }
+            }
         }
-        .padding()
     }
 }
-
-#Preview {
+#Preview ("List With Sample Data"){
+    let container = try! ModelContainer(for:Wish.self,configurations:ModelConfiguration(isStoredInMemoryOnly: true))
+    container.mainContext.insert(Wish(title: "Master in SwiftUI"))
+    container.mainContext.insert(Wish(title: "Buy a new iphone "))
+    container.mainContext.insert(Wish(title: "Make a positive impact"))
+    return ContentView()
+        .modelContainer(container)
+}
+#Preview ("Empty list"){
     ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
 }
--- Wishlist/WishModel.swift
+++ Wishlist/WishModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftData
+@Model
+class Wish{
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
